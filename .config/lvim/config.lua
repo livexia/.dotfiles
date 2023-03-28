23aa58dc16a012a7d10309dd13ec29e70e83c079
livexia <istillalive@live.cn>
2023-03-29 01:44:32 +0800

    lvim: add rust-tool

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -180,6 +180,9 @@ lvim.plugins = {
   {
     "arcticicestudio/nord-vim",
   },
+  {
+    "simrat39/rust-tools.nvim"
+  }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -195,3 +198,17 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+
+-- rust-tool (https://github.com/simrat39/rust-tools.nvim)
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})

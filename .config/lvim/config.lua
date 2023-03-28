ce7be0eaf5d90db949d08861bbf6bc26792071bf
livexia <istillalive@live.cn>
2023-03-29 02:23:00 +0800

    lvim: add vim-fugitive

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -182,7 +182,26 @@ lvim.plugins = {
   },
   {
     "simrat39/rust-tools.nvim"
-  }
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)

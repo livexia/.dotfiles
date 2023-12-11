cfa95dbd548939cff736954fb3d5ad9f4f1b4aa2
livexia <istillalive@live.cn>
2023-12-11 16:05:28 +0800

    more config for rust-tools

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -196,6 +196,8 @@ lvim.plugins = {
     {
         "simrat39/rust-tools.nvim"
     },
+    { "nvim-lua/plenary.nvim" },
+    { "mfussenegger/nvim-dap" },
     {
         "tpope/vim-fugitive",
         cmd = {
@@ -245,7 +247,7 @@ rt.setup({
             -- Hover actions
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
             -- Code action groups
-            -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
         end,
     },
 })

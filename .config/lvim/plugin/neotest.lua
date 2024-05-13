6c14f53cce188b02f51590fd5c44572989415843
livexia <istillalive@live.cn>
2024-05-13 22:52:54 +0800

    lvim: safe load neotest

--- .config/lvim/plugin/neotest.lua
+++ .config/lvim/plugin/neotest.lua
@@ -15,38 +15,46 @@ wk.register({
   },
 }, { prefix = "<leader>" })
 
----Neotest is quite heavy on startup, so this initializes it lazily, on keymap
----@diagnostic disable-next-line: missing-fields
-neotest.setup {
-  adapters = {
-    require("rustaceanvim.neotest"),
-    require("neotest-python") {
-      -- Extra arguments for nvim-dap configuration
-      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-      dap = {
-        justMyCode = false,
-        console = "integratedTerminal",
-      },
-      args = { "--log-level", "DEBUG", "--quiet" },
-      runner = "pytest",
-    },
-  },
+local function neotest_setup()
+  ---Neotest is quite heavy on startup, so this initializes it lazily, on keymap
   ---@diagnostic disable-next-line: missing-fields
-  discovery = {
-    enabled = true,
-  },
-  icons = {
-    failed = "",
-    passed = "",
-    running = "",
-    skipped = "",
-    unknown = "",
-  },
-  quickfix = {
-    enabled = false,
-    open = false,
-  },
-}
+  neotest.setup {
+    adapters = {
+      require("rustaceanvim.neotest"),
+      require("neotest-python") {
+        -- Extra arguments for nvim-dap configuration
+        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+        dap = {
+          justMyCode = false,
+          console = "integratedTerminal",
+        },
+        args = { "--log-level", "DEBUG", "--quiet" },
+        runner = "pytest",
+      },
+    },
+    ---@diagnostic disable-next-line: missing-fields
+    discovery = {
+      enabled = true,
+    },
+    icons = {
+      failed = "",
+      passed = "",
+      running = "",
+      skipped = "",
+      unknown = "",
+    },
+    quickfix = {
+      enabled = false,
+      open = false,
+    },
+  }
+end
+
+local status, err = pcall(neotest_setup)
+if not status then
+  vim.notify("Could not setup module neotest: " .. err, "error", { title = "Neotest Setup Failed" })
+  return
+end
 
 vim.keymap.set("n", "<leader>nr", neotest.run.run, desc("[n]eotest: [r]un nearest"))
 vim.keymap.set("n", "<leader>nR", function()

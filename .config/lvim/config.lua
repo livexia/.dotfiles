9817387c90e0b787bf8db445c8e1b1ab368f4e36
livexia <istillalive@live.cn>
2024-05-14 17:33:16 +0800

    lvim: adding notify plugin for better notify

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -154,6 +154,35 @@ lvim.plugins = {
     "ckipp01/stylua-nvim",
     build = "cargo install stylua",
   },
+  {
+    -- see: https://www.lazyvim.org/plugins/ui#nvim-notify
+    "rcarriga/nvim-notify",
+    -- keys = {
+    --   {
+    --     "<leader>un",
+    --     function()
+    --       require("notify").dismiss { silent = true, pending = true }
+    --     end,
+    --     desc = "Dismiss All Notifications",
+    --   },
+    -- },
+    opts = {
+      stages = "static",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+    init = function()
+      vim.notify = require("notify")
+    end,
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)

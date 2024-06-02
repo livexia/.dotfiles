e590fd237f54dc0fcc6fd57526ea2ddb54b28b3c
livexia <istillalive@live.cn>
2024-06-02 21:04:44 +0800

    wezterm: change window_padding, font_size and color_scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,4 +6,13 @@ config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.enable_wayland = false
 config.front_end = "OpenGL"
 
+config.window_padding = {
+  left = "0.4cell",
+  right = "0.3cell",
+  top = "0.3cell",
+  bottom = "0cell",
+}
+
+config.font_size = 14.0
+config.color_scheme = "Argonaut (Gogh)"
 return config

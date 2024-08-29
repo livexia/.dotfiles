a10f265218c410f924c2b2e2de7f97f558cfecec
livexia <istillalive@live.cn>
2024-08-29 17:37:15 +0800

    lvim: adding snippet to enable qmlls, and set keymap for enter normal mode terminal

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -396,3 +396,10 @@ api.nvim_create_autocmd({ "BufRead", "BufReadPost" }, {
 -- enable jsonls
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#jsonls
 require("lspconfig").jsonls.setup {}
+
+-- enable qmlls
+-- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#qmlls
+require("lspconfig").qmlls.setup {}
+
+-- set shortcut for enter normal mode in terminal
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { silent = true, noremap = true })

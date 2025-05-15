c4a2f6f5e24b5677d845c40d03791288ba58b93b
livexia <istillalive@live.cn>
2025-05-16 02:31:29 +0800

    nvim: adding which-key

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,22 @@
+return {
+  -- which-key helps you remember key bindings by showing a popup
+  -- with the active keybindings of the command you started typing.
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show { global = false }
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+}

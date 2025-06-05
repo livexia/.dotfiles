a4c4c147746be37cdc7bfe3152e4b13f965951b7
livexia <istillalive@live.cn>
2025-06-05 23:57:04 +0800

    nvim: which-key adding more options

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -3,11 +3,26 @@ return {
   -- with the active keybindings of the command you started typing.
   {
     "folke/which-key.nvim",
+    opts_extend = { "spec" },
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      preset = "helix",
+      defaults = {},
+      spec = {
+        {
+          mode = { "n", "v" },
+          {
+            "<leader>w",
+            group = "windows",
+            proxy = "<c-w>",
+            expand = function()
+              return require("which-key.extras").expand.win()
+            end,
+          },
+          -- better descriptions
+          { "gx", desc = "Open with system app" },
+        },
+      },
     },
     keys = {
       {
@@ -16,6 +31,13 @@ return {
           require("which-key").show { global = false }
         end,
         desc = "Buffer Local Keymaps (which-key)",
+      },
+      {
+        "<c-w><space>",
+        function()
+          require("which-key").show { keys = "<c-w>", loop = true }
+        end,
+        desc = "Window Hydra Mode (which-key)",
       },
     },
   },
